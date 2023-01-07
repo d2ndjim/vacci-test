adbed664fd6df7b93a3dc687acc38673d8dbe4f4
Lekan <jimoridwanlekan@gmail.com>
2023-01-07 20:16:22 +0100

    Modify guardian controller

--- app/controllers/guardians_controller.rb
+++ app/controllers/guardians_controller.rb
@@ -1,15 +1,39 @@
 class GuardiansController < ApplicationController
   before_action :load_guardian
 
+  def update
+    if admin?
+      @guardian.update(guardian_params)
+      if @guardian.update(update_params)
+        render json: { message: 'guardian updated', status: :updated }
+      else
+        render json: { message: 'Email already exists' }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Not Allowed' }, status: :unauthorized
+    end
+  end
+
   def destroy
-    @guardian = Guardian.find(params[:id])
-    @guardian.destroy
-    head :no_content
+    if admin?
+      @guardian = Guardian.find(params[:id])
+      if @guardian.destroy
+        render json: { message: 'guardian deleted', status: :destroyed }, status: :ok
+      else
+        render json: { message: 'guardian not deleted' }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Not Allowed' }, status: :unauthorized
+    end
   end
 
   private
 
   def load_guardian
     @guardian = Guardian.includes(:children).find(params[:id])
+  end
+
+  def guardian_params
+    params.permit(:first_name, :last_name, :relationship, :email, :phone, :location, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :wards
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  get "/authorized", to: "sessions#show"
+  post "/create/guardian", to: "users#add_guardian"
+  delete "guardians/:id", to: "guardians#destroy"
+  
 end
