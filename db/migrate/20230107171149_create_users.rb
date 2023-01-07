c5d85a08347fa536c6e928f6287f857c3e229f3d
Lekan <jimoridwanlekan@gmail.com>
2023-01-07 18:12:06 +0100

    Add user resource

---
+++ db/migrate/20230107171149_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :phone
+      t.string :location
+      t.string :relationship
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
