ca5d4072a53f743cb2d33809dbdb7d1fd2f961bb
Lekan <jimoridwanlekan@gmail.com>
2023-01-16 17:34:47 +0100

    Remove phone migration

--- db/migrate/20230107172322_create_wards.rb
+++ db/migrate/20230107172322_create_wards.rb
@@ -1,12 +1,12 @@
 class CreateWards < ActiveRecord::Migration[7.0]
   def change
     create_table :wards do |t|
-      t.string :first_name
-      t.string :last_name
-      t.date :DOB
-      t.string :gender
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.date :DOB, null: false
+      t.string :gender, null: false
       t.decimal :height
-      t.integer :weight
+      t.decimal :weight
 
       t.timestamps
     end
