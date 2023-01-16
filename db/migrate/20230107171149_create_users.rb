a31a63a96060778da5dfb97223285cec1bcad8eb
Lekan <jimoridwanlekan@gmail.com>
2023-01-16 17:34:26 +0100

    Update users migration to not allow null values

--- db/migrate/20230107171149_create_users.rb
+++ db/migrate/20230107171149_create_users.rb
@@ -1,13 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :password_digest
-      t.string :phone
-      t.string :location
-      t.string :relationship
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :location, null: false,
+      t.string :relationship, null: false
       t.references :guardian, foreign_key: { to_table: :users }
 
       t.timestamps
