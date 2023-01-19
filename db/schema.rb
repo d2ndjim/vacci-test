cb7fe9e6127558a16287bde10121bc4e8587ea7f
Lekan <jimoridwanlekan@gmail.com>
2023-01-19 15:12:00 +0100

    Modify schema with rails migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_07_181759) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_134911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "immunizations", force: :cascade do |t|
+    t.date "w6"
+    t.date "w10"
+    t.date "w14"
+    t.date "m6"
+    t.date "m9"
+    t.date "m12"
+    t.date "m15"
+    t.bigint "ward_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ward_id"], name: "index_immunizations_on_ward_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
@@ -47,5 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_181759) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "immunizations", "wards"
   add_foreign_key "users", "users", column: "guardian_id"
 end
