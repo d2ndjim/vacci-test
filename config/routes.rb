578b0e57ebcb583fdf723b04c985bc2af2ab8803
d2ndjim <jimohridwanlekan@gmail.com>
2023-01-28 15:21:56 +0100

    Configure cloudinary and activestorage for project

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
   resources :immunizations
+
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   post "/guardian", to: "guardians#create"
   post "/ward", to: "wards#create"
-  post "/upload/:id/:avatar", to: "wards#upload_avatar"
-  post "/upload/:avatar", to: "users#upload_avatar"
+  post "/upload/avatar", to: "users#upload_avatar"
   get "/authorized", to: "sessions#show"
   get "/wards", to: "wards#index"
   get "/guardians", to: "users#user_guardians"
