9f9a424ab4cf6cd273cceef950c043a410be374b
d2ndjim <jimohridwanlekan@gmail.com>
2023-02-02 23:22:05 +0100

    Add routing for updating immunizatios

--- app/sidekiq/immunization_schedule_reminder_job.rb
+++ app/sidekiq/immunization_schedule_reminder_job.rb
@@ -2,9 +2,14 @@ class ImmunizationScheduleReminderJob
   include Sidekiq::Job
 
   def perform()
-    immunizations = Immunization.where(vaccination_date: Date.today + 1.day)
-    immunizations.each do |immunization|
+    immunizations = Immunization.all
+    filtered_immunizations = immunizations.select do |immunization|
+      immunization.vaccination_date == Date.today + immunization.reminder_days.days
+    end
+    filtered_immunizations.each do |immunization|
       ImmunizationScheduleMailer.reminder_email(immunization.ward.user, immunization.ward, immunization.vaccination_date).deliver_now
     end
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,12 @@ Rails.application.routes.draw do
 
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
-  post "/guardian", to: "guardians#create"
   post "/ward", to: "wards#create"
   post "/upload/avatar", to: "users#upload_avatar"
   get "/authorized", to: "sessions#show"
   get "user/wards", to: "wards#index"
-  get "/guardians", to: "users#user_guardians"
+  get "/immunizations", to: "immunizations#index"
   get "/upcoming", to: "immunizations#upcoming"
-  delete 'guardian/:guardian_id', to: 'guardians#destroy'
-  patch "/guardians/:id", to: "guardians#update"
   patch "/ward", to: "wards#update"
   patch "/immunization", to: "immunizations#update"
   delete "/ward/:id", to: "wards#destroy"
