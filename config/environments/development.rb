99520a8774d6631525831816f47e388108380be1
d2ndjim <jimohridwanlekan@gmail.com>
2023-02-04 12:25:47 +0100

    Configure smtp for action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'vaccitracker@gmail.com',
+    :password       => 'uoqpegerewnsdzlp',
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
 
 
   # Print deprecation notices to the Rails logger.
