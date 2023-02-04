5d8e6698d379ef917f96aabe36808617d439c617
d2ndjim <jimohridwanlekan@gmail.com>
2023-02-04 12:25:30 +0100

    Add serializer for vaccination tracker

--- app/sidekiq/immunization_schedule_reminder_job.rb
+++ app/sidekiq/immunization_schedule_reminder_job.rb
@@ -6,8 +6,9 @@ class ImmunizationScheduleReminderJob
     filtered_immunizations = immunizations.select do |immunization|
       immunization.vaccination_date == Date.today + immunization.reminder_days.days
     end
+    # binding.pry
     filtered_immunizations.each do |immunization|
-      ImmunizationScheduleMailer.reminder_email(immunization.ward.user, immunization.ward, immunization.vaccination_date).deliver_now
+      ImmunizationScheduleMailer.reminder_email(immunization.ward.user, immunization.ward).deliver_now
     end
   end
 end
