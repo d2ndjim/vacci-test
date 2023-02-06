8aff95f0ef70532c8871a2897841c34c850e6ef8
d2ndjim <jimohridwanlekan@gmail.com>
2023-02-06 16:48:24 +0100

    Remove api key from public repository

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
-  :password => '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',
+  :password => '',
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
