b871d4337a347d8f23c7022b7b51148c8610f943
d2ndjim <jimohridwanlekan@gmail.com>
2023-02-07 15:48:36 +0100

    Add requests test for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
   resources :immunizations
