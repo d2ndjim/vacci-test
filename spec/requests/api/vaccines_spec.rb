42e8fb3d738779607411a07e4bea3a2abdcbb33c
d2ndjim <jimohridwanlekan@gmail.com>
2023-02-07 16:20:22 +0100

    Add request tests for vaccines

---
+++ spec/requests/api/vaccines_spec.rb
@@ -0,0 +1,35 @@
+require 'swagger_helper'
+
+RSpec.describe 'vaccines', type: :request do
+  path '/vaccines' do
+    get('index vaccine') do
+      tags 'Vaccines'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/upcoming' do
+    get('upcoming immunization') do
+      tags 'Immunizations'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
