5759227e576702687461a9c4aeaa583d25b59787
d2ndjim <jimohridwanlekan@gmail.com>
2023-02-07 16:12:34 +0100

    Add immunization and session specs

---
+++ spec/requests/api/immunizations_spec.rb
@@ -0,0 +1,35 @@
+require 'swagger_helper'
+
+RSpec.describe 'immunizations', type: :request do
+  path '/upcoming' do
+    get('upcoming immunization') do
+      tags 'Immunizations'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/immunization' do
+    patch('update immunization') do
+      tags 'Immunizations'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
